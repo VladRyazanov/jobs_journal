3529ffe71d683ad805eb902b5037a74103e62f47
vlad <v1adryazanov@yandex.com>
2024-02-13 18:24:02 +0300

    Обработчик формы авторизации

--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 
@@ -7,10 +7,17 @@ class RegisterForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    surname = StringField('Имя пользователя', validators=[DataRequired()])
+    surname = StringField('Фамилия пользователя', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
     age = StringField("Возраст", validators=[DataRequired()])
     position = StringField("Должность", validators=[DataRequired()])
     speciality = StringField("Специальность", validators=[DataRequired()])
     address = StringField("Адрес", validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from data import db_session
 from data.jobs import Jobs
 from data.users import User
 from flask import Flask, render_template, redirect
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/data.db")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route("/")
@@ -55,10 +65,27 @@ def register():
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
-#
-# @app.route("/login")
-# def login():
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == "__main__":
